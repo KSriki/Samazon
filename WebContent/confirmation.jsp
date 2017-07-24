067a7ef65157106e562b8b9ad286b746d1cb1d80
allysa adams <allysa_adams@yahoo.com>
2017-07-24 16:23:41 -0400

    added confirmation

--- WebContent/confirmation.jsp
+++ WebContent/confirmation.jsp
@@ -1,12 +1,23 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+ <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<fmt:setLocale value="en_US" />
+    
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
+<jsp:include page="include.jsp" />
+
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<title>Confirmation Page</title>
 </head>
 <body>
 
+	<jsp:include page="navbar.jsp" />
+	<c:out value="${total}"/>
+	<c:out value="${user.}"/>
+
 </body>
 </html>
+
--- src/servlets/ConfirmationServlet.java
+++ src/servlets/ConfirmationServlet.java
@@ -1,6 +1,8 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.Samitem;
+import model.Samuser;
 import util.DbItems;
 
 /**
@@ -29,37 +33,61 @@ public class ConfirmationServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		doPost (request,response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		
+
 		String nextURL = "/confirmation.jsp";
+	
+		
+		
 		HttpSession session = request.getSession();
 
 		//working to change code below do not change
 		
-		/*session.setAttribute("cartitem", DbItems.getCartitem(sam.getSamid()));
+		Samuser sam = (Samuser) session.getAttribute("user");
 		
-		response.sendRedirect(request.getContextPath() + nextURL);
-		doGet(request, response);
+		
+
+		if (session.getAttribute("user")==null){
+			//http://stackoverflow.com/questions/13638446/checking-servlet-session-attribute-value-in-jsp-file
+			nextURL = "/login.jsp";
+			response.sendRedirect(request.getContextPath() + nextURL);
+return;//return prevents an error; Don't believe me? Take it out.
+	}else{
+		nextURL = "/confirmation.jsp";
 	}
-	public void Add(HttpSession session ,double price){
+		
 
-	    Double sum=(Double)session.getAttribute("sum");
-	    if(sum==null){
-	        sum = new Double(price);
-	    }
-	        else {
-	        sum = new Double(sum.intValue()+ price);
-	    }
-	    session.setAttribute("sum", sum);
-	    }*/
+			
+		List<Samitem> mycart = DbItems.getCartitems(sam.getSamid());
+	
+		float total = 0;
+		
+		System.out.println(total);
+		for (Samitem item : mycart){
+			float price = item.getPrice();
+			int quantity = item.getQuantity();
+			float subtotal;
+		
+			subtotal = price * quantity;
+			total = total + subtotal;
+		}
+		System.out.println(sam);
+		System.out.println(total);
+		
+		request.setAttribute("total", total);
+		getServletContext().getRequestDispatcher(nextURL).forward(request,response);
 
+
+	}
+	
+			
+		
+		
 }
